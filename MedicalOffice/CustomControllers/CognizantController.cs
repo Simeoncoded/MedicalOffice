1c8c2d390b336768fa7cafa1a9eafe7af9d6c9e4
Simeoncoded <simeonolawore07@gmail.com>
2024-10-11 17:14:15 -0400

    added new custom controllers

--- MedicalOffice/CustomControllers/CognizantController.cs
+++ MedicalOffice/CustomControllers/CognizantController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace MedicalOffice.CustomControllers
 {
@@ -11,11 +12,62 @@ namespace MedicalOffice.CustomControllers
     {
         internal string ControllerName()
         {
-            return ControllerContext.RouteData.Values["controller"]?.ToString() ?? string.Empty;
+            return ControllerContext.RouteData.Values["controller"].ToString();
         }
         internal string ActionName()
         {
-            return ControllerContext.RouteData.Values["action"]?.ToString() ?? string.Empty;
+            return ControllerContext.RouteData.Values["action"].ToString();
+        }
+
+        /// <summary>
+        /// This method uses a regular expression to split the input string at each 
+        /// capital letter and then adds a space before each capital letter. 
+        /// The Trim() method is used to remove any leading or trailing spaces.
+        /// </summary>
+        /// <param name="input">String in camel case to split into words</param>
+        /// <returns>The camel case String split into words</returns>
+        internal static string SplitCamelCase(string input)
+        {
+            return System.Text.RegularExpressions.Regex
+                .Replace(input, "([A-Z])", " $1",
+                System.Text.RegularExpressions.RegexOptions.Compiled).Trim();
+        }
+
+        /// <summary>
+        /// This code is executed before any Action method is called
+        /// and gives us a chance to add to the ViewData dictionary.
+        /// Great way to make these values available to all Views.
+        /// </summary>
+        /// <param name="context"></param>
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            //Add the Controller and Action names to ViewData
+            string ControllerFriendlyName = SplitCamelCase(ControllerName());
+            ViewData["ControllerName"] = ControllerName();
+            ViewData["ControllerFriendlyName"] = ControllerFriendlyName;
+            ViewData["ActionName"] = ActionName();
+            ViewData["Title"] = ControllerFriendlyName + " " + ActionName();
+            base.OnActionExecuting(context);
+        }
+
+        /// <summary>
+        /// Same as above but for async Actions
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="next"></param>
+        /// <returns></returns>
+        public override Task OnActionExecutionAsync(
+            ActionExecutingContext context,
+            ActionExecutionDelegate next)
+        {
+            //Add the Controller and Action names to ViewData
+            string ControllerFriendlyName = SplitCamelCase(ControllerName());
+            ViewData["ControllerName"] = ControllerName();
+            ViewData["ControllerFriendlyName"] = ControllerFriendlyName;
+            ViewData["ActionName"] = ActionName();
+            ViewData["Title"] = ControllerFriendlyName + " " + ActionName();
+            return base.OnActionExecutionAsync(context, next);
         }
     }
+
 }
---
+++ MedicalOffice/CustomControllers/ElephantController.cs
@@ -0,0 +1,49 @@
+﻿using MedicalOffice.Utilities;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MedicalOffice.CustomControllers
+{
+    /// <summary>
+    /// The Elephant Controller has a good memory to help
+    /// persist the Index Sort, Filter and Paging parameters
+    /// into a URL stored in ViewData
+    /// WARNING: Depends on the following Utilities
+    ///  - CookieHelper
+    ///  - MaintainURL
+    /// </summary>
+    public class ElephantController : CognizantController
+    {
+        //This is the list of Actions that will add the ReturnURL to ViewData
+        internal string[] ActionWithURL = [ "Details", "Create", "Edit", "Delete",
+            "Add", "Update", "Remove" ];
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (ActionWithURL.Contains(ActionName()))
+            {
+                ViewData["returnURL"] = MaintainURL.ReturnURL(HttpContext, ControllerName());
+            }
+            else if (ActionName() == "Index")
+            {
+                //Clear the sort/filter/paging URL Cookie for Controller
+                CookieHelper.CookieSet(HttpContext, ControllerName() + "URL", "", -1);
+            }
+            base.OnActionExecuting(context);
+        }
+
+        public override Task OnActionExecutionAsync(
+            ActionExecutingContext context,
+            ActionExecutionDelegate next)
+        {
+            if (ActionWithURL.Contains(ActionName()))
+            {
+                ViewData["returnURL"] = MaintainURL.ReturnURL(HttpContext, ControllerName());
+            }
+            else if (ActionName() == "Index")
+            {
+                //Clear the sort/filter/paging URL Cookie for Controller
+                CookieHelper.CookieSet(HttpContext, ControllerName() + "URL", "", -1);
+            }
+            return base.OnActionExecutionAsync(context, next);
+        }
+    }
+}
